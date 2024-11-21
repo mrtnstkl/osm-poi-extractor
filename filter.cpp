mrtnstkl/osm-poi-extractor

commit 8dfc15fe23384a741d6629e6253b8d989023d746

support processing of unnamed features

filter.cpp
@@ -42,10 +42,17 @@ filter filter::parse_args(const std::vector<std::string>& args)
 	return filter;
 }
 
+void filter::add_rule(const rule& rule)
+{
+	rule_list_.push_back(rule);
+}
+void filter::add_rule(const key& k, const value& v)
+{
+	rule_list_.push_back({{k, v}});
+}
+
 bool filter::check(const osmium::TagList &tags) const
 {
-	if (!tags["name"])
-		return false;
 	if (rule_list_.empty())
 		return true;
 

filter.h
@@ -14,6 +14,8 @@ class filter
 
 public:
 	static filter parse_args(const std::vector<std::string>& args);
+	void add_rule(const rule& rule);
+	void add_rule(const key& k, const value& v = std::string());
 	bool check(const osmium::TagList &tags) const;
 	void print(std::ostream &out) const;
 };

main.cpp
@@ -10,7 +10,7 @@
 #include "node-handler.h"
 #include "filter.h"
 
-#define PRINT_USAGE(argv0) std::cerr << "Usage: " << argv0 << " INFILE OUTFILE [-p | -a] [FILTERS...]" << std::endl
+#define PRINT_USAGE(argv0) std::cerr << "Usage: " << argv0 << " INFILE OUTFILE [-p | -a] [-un] [FILTERS...]" << std::endl
 
 template <typename... T>
 void process(osmium::io::File &file, T &...handlers)
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
 
 	std::vector<std::string> filter_args;
 	bool process_ways = false, process_nodes = false;
+	bool allow_unnamed = false;
 	for (int i = 3; i < argc; ++i)
 	{
 		std::string arg(argv[i]);
@@ -38,6 +39,8 @@ int main(int argc, char *argv[])
 			process_ways = true;
 		else if (arg == "-p")
 			process_nodes = true;
+		else if (arg == "-un")
+			allow_unnamed = true;
 		else if (arg[0] == '-')
 		{
 			std::cerr << "Invalid argument \"" << arg << '"' << std::endl;
@@ -68,6 +71,8 @@ int main(int argc, char *argv[])
 	}
 
 	auto filter = filter::parse_args(filter_args);
+	if (!allow_unnamed)
+		filter.add_rule("name");
 	filter.print(std::cout);
 
 	poly_map poly_map;

node-handler.cpp
@@ -29,14 +29,14 @@ coord poly_map::poly_position(poly_id poly_id)
 
 std::string default_formatter::node(const osmium::Node &node)
 {
-	poi poi(node.tags()["name"], node.location().lat(), node.location().lon(), node.id());
+	poi poi(node.location().lat(), node.location().lon(), node.id());
 	poi.set_tags(node.tags());
 	return poi.string();
 }
 
 std::string default_formatter::way(const osmium::Way &way, coord coordinates)
 {
-	poi poi(way.tags()["name"], coordinates.lat, coordinates.lon, way.id());
+	poi poi(coordinates.lat, coordinates.lon, way.id());
 	poi.set_tags(way.tags());
 	return poi.string();
 }

poi.cpp
@@ -22,21 +22,27 @@ coord poly_coord::get()
 	return {(lat_min_ + (lat_max_ - lat_min_) * .5f), (lon_min_ + (lon_max_ - lon_min_) * .5f)};
 }
 
-poi::poi(const char *name, float lat, float lon, nlohmann::json tags)
-	: json_({{"name", name}, {"lat", lat}, {"lon", lon}, {"tags", tags}})
+poi::poi(float lat, float lon, nlohmann::json tags)
+	: json_({{"lat", lat}, {"lon", lon}, {"tags", tags}})
 {
 }
 
-poi::poi(const char *name, float lat, float lon, osmium::object_id_type id, nlohmann::json tags)
-	: json_({{"name", name}, {"id", id}, {"lat", lat}, {"lon", lon}, {"tags", tags}})
+poi::poi(float lat, float lon, osmium::object_id_type id, nlohmann::json tags)
+	: json_({{"id", id}, {"lat", lat}, {"lon", lon}, {"tags", tags}})
 {
+	if (tags.contains("name"))
+		json_["name"] = tags["name"];
 }
 
 void poi::set_tags(const osmium::TagList &tag_list)
 {
 	auto& tags = json_["tags"];
 	for (const auto& tag : tag_list)
+	{
 		tags[tag.key()] = tag.value();
+		if (tag.key() == std::string("name"))
+			json_["name"] = tag.value();
+	}
 }
 
 poi poi::parse_geoname(const std::string& line)
@@ -57,12 +63,7 @@ poi poi::parse_geoname(const std::string& line)
 	ss >> longitude;
 	ss.ignore();
 
-	return poi(name.c_str(), latitude, longitude);
-}
-
-const std::string& poi::name() const
-{
-	return json_["name"].template get_ref<const std::string&>();
+	return poi(latitude, longitude, {"name", name});
 }
 
 float poi::lat() const

poi.h
@@ -30,14 +30,13 @@ class poi
 	nlohmann::json json_;
 
 public:
-	poi(const char* name, float lat, float lon, nlohmann::json tags = nlohmann::json::object());
-	poi(const char* name, float lat, float lon, osmium::object_id_type id, nlohmann::json tags = nlohmann::json::object());
+	poi(float lat, float lon, nlohmann::json tags = nlohmann::json::object());
+	poi(float lat, float lon, osmium::object_id_type id, nlohmann::json tags = nlohmann::json::object());
 
 	void set_tags(const osmium::TagList &tag_list);
 
 	static poi parse_geoname(const std::string& line);
 
-	const std::string& name() const;
 	float lat() const;
 	float lon() const;
 

commit e6f2018a1a0eade95143cafcc16f1d4d166c545b

fix name not being treated as a hard requirement
broken by 8dfc15fe

filter.cpp
@@ -51,8 +51,16 @@ void filter::add_rule(const key& k, const value& v)
 	rule_list_.push_back({{k, v}});
 }
 
+void filter::allow_unnamed(bool allow_unnamed)
+{
+	allow_unnamed_ = allow_unnamed;
+}
+
 bool filter::check(const osmium::TagList &tags) const
 {
+	if (!allow_unnamed_ && !tags["name"])
+		return false;
+
 	if (rule_list_.empty())
 		return true;
 
@@ -76,6 +84,8 @@ bool filter::check(const osmium::TagList &tags) const
 
 void filter::print(std::ostream &out) const
 {
+	if (allow_unnamed_)
+		out << "Unnamed POIs are allowed\n";
 	if (rule_list_.empty())
 	{
 		out << "Using empty filter" << std::endl;

filter.h
@@ -11,11 +11,13 @@ class filter
 	using rule = std::vector<tag>;
 
 	std::vector<rule> rule_list_;
+	bool allow_unnamed_ = false;
 
 public:
 	static filter parse_args(const std::vector<std::string>& args);
 	void add_rule(const rule& rule);
 	void add_rule(const key& k, const value& v = std::string());
+	void allow_unnamed(bool allow_unnamed);
 	bool check(const osmium::TagList &tags) const;
 	void print(std::ostream &out) const;
 };

main.cpp
@@ -71,8 +71,7 @@ int main(int argc, char *argv[])
 	}
 
 	auto filter = filter::parse_args(filter_args);
-	if (!allow_unnamed)
-		filter.add_rule("name");
+	filter.allow_unnamed(allow_unnamed);
 	filter.print(std::cout);
 
 	poly_map poly_map;